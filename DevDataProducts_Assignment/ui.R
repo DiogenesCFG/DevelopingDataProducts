fee5df71ee315c6911271fc14a34c592c588f656
DiogenesCFG <diogenescfg@gmail.com>
2020-03-08 14:42:27 -0600

    Adding better instructions for app usage.

--- DevDataProducts_Assignment/ui.R
+++ DevDataProducts_Assignment/ui.R
@@ -3,7 +3,9 @@ shinyUI(navbarPage("Optimizing Common Utility Functions",
    tabPanel("Cobb Douglas",
             h4("We'll use a Cobb Douglas utility function of the form:"), 
             h3(withMathJax('$$U(X, Y) = X ^ \\alpha Y ^ {1 - \\alpha}$$')), 
-            h4(withMathJax("Where $$0 < \\alpha < 1$$")), 
+            h4(withMathJax("Where $$0 < \\alpha < 1$$")),
+            h4("Go ahead and change the alpha parameter, as well as the prices 
+               and income to see how the optimal consumption basket changes"),
             sidebarPanel(
                 sliderInput("cd_alpha", "Select the value of alpha", value = 1/2, 
                             min = 0.01, max = 0.99, step = .01), 
@@ -21,9 +23,10 @@ shinyUI(navbarPage("Optimizing Common Utility Functions",
    ),
    tabPanel("Quasilinear",
             h4("We'll use a quasilinear utility function of the form:"), 
-            h3(withMathJax('$$U(X, Y) = X ^ \\alpha + 12Y$$')),  
+            h3(withMathJax('$$U(X, Y) = X  + 12Y^ \\alpha$$')),  
             h4(withMathJax("Where $$0 < \\alpha < 1$$")), 
-            h4("The 400 is just a scalar so the graph looks better"),  
+            h4("Go ahead and change the prices, income or alpha parameter 
+               to see how the optimal consumption basket changes"),  
             sidebarPanel(
                 sliderInput("ql_alpha", "Select the value of alpha", value = 1/2, 
                             min = 0.01, max = 0.99, step = .01), 
@@ -43,15 +46,18 @@ shinyUI(navbarPage("Optimizing Common Utility Functions",
             h4("We'll use a Constant Elasticity of Substitution (CES) utility function of the form:"), 
             h3(withMathJax('$$U(X,Y) = \\alpha X ^ {-\\rho} + (1-\\alpha)Y ^ {-\\rho}$$')), 
             h4(withMathJax("when $$\\rho < [-1, 0) \\cap (0, \\infty)$$")), 
-            h4("Although the CES specification does not allow for rho to equal zero, as it will 
-               imply an infinite exponential, we'll allow the option, considering that when rho equals 
-               zero, the CES function converges to a Cobb Douglas function"), 
+            h4("Go ahead and change the alpha parameter, as well as the prices 
+               and income to see how the optimal consumption basket changes. 
+               You can also change the rho parameter, and see how the curve looks 
+               like a perfect substitute when rho equals one, like a cobb douglas as 
+               rho approaches zero, or a perfect compliments curve as rho increases
+               to 100."), 
             sidebarPanel(
                sliderInput("ces_alpha", "Select the value of alpha", value = 1/2, 
                            min = 0.01, max = 0.99, step = .01), 
                selectInput("ces_rho", "Select the value of rho",
-                           choices = list(-1, -.9, -.85, -.80, -.75, -.5, -.25, 
-                                          .25, .5, .75, 1, 1.25, 1.5, 1.75, 2, 5, 10, 100)), 
+                           choices = list(-1, -.9, -.85, -.80, -.75, -.5, -.25, -.1, -.01, 
+                                          .01, .1, .25, .5, .75, 1, 1.25, 1.5, 1.75, 2, 5, 10, 100)), 
                sliderInput("ces_Income", "Select Value of Income", value = 100, 
                            min = 50, max = 200, step = 1), 
                sliderInput("ces_Px", "Select Price of X", value = 1, 
